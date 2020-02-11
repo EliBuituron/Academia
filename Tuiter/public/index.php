7b79c074b7213940574d3b193c934476e362b4ab
EliBuituron <eliel.buituron@gmail.com>
2020-02-11 10:00:07 -0300

    primer push al branch de templates

--- Tuiter/public/index.php
+++ Tuiter/public/index.php
@@ -29,23 +29,75 @@ $loginService = new \Tuiter\Services\LoginService($userService);
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, array $args) use ($twig) {
-    
     $template = $twig->load('index.html');
-
     $response->getBody()->write(
-        $template->render(['name' => 'Dario'])
+        $template->render()
     );
     return $response;
 });
 
-$app->get('/contacto', function (Request $request, Response $response, array $args) use ($twig) {
-    
-    $template = $twig->load('contacto.html');
+$app->post('/', function (Request $request, Response $response, array $args) use ($loginService) {
+    $loginService->login($_POST['username'],$_POST['password']);
+    if($_SESSION['login'] == true){
+        $response = $response->withStatus(302);
+        return $response->withHeader("Location", "/feed");
+    }else{
+        $response = $response->withStatus(302);
+        return $response->withHeader("Location", "/");
+    }
+});
 
+$app->get('/register', function (Request $request, Response $response, array $args) use ($twig) {
+    $template = $twig->load('register.html');
     $response->getBody()->write(
-        $template->render(['name' => 'Dario'])
+        $template->render()
     );
     return $response;
+});
+
+$app->post('/register', function (Request $request, Response $response, array $args) use ($userService) {
+    $result = $userService->register($_POST['username'],$_POST['username'],$_POST['password']);
+    if($result == true){
+        $response = $response->withStatus(302);
+        return $response->withHeader("Location", "/");
+    }else{
+        $response = $response->withStatus(302);
+        return $response->withHeader("Location", "/register");
+    }
+});
+
+$app->get('/logout', function (Request $request, Response $response, array $args) use ($loginService){
+    $loginService->logout();
+    $response = $response->withStatus(302);
+    return $response->withHeader("Location", "/");
+});
+
+$app->get('/feed', function (Request $request, Response $response, array $args) use ($twig, $followService, $postService, $userService) {
+    $template = $twig->load('feed.html');
+    $followed = $followService->getFollowed($_SESSION['user']);
+    $postObjects = array();
+    $posts = array();
+
+    foreach($followed as $user){
+        $postObjects[] = $postService->getAllPosts($user);
+        foreach ($postObjects as $post){
+            $posts[] = array(
+                $post['owner'],
+                $post['content']
+            );
+        }
+    }
+
+    $response->getBody()->write(
+        $template->render()
+    );
+    return $response;
+});
+
+$app->get('/user/me', function (Request $request, Response $response, array $args) use ($twig){
+
+    $response = $response->withStatus(302);
+    return $response->withHeader("Location", "/");
 });
 
 $app->run();
